0e297d376f3bcffaf862cce73e20c042dd3e9064
Enrique Esquinas <esquinas.enrique@gmail.com>
2018-08-04 16:28:04 +0200

    Undoing.

--- simple-contact-us-handler/viewer.js
+++ simple-contact-us-handler/viewer.js
@@ -1,7 +1,7 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
-    let searchDate = event.queryStringParameters.date;
+	let searchDate = event.queryStringParameters.date;
 	let response = {
 		body: "",
 		statusCode: 200,
@@ -15,7 +15,7 @@ exports.handler = function (event, context, callback) {
 		FilterExpression: 'entryDate = :filterDate'
 	}, function (err, data) {
 		if (err) {
-			response.body = JSON.stringify(err.errorMessage);
+			response.body = JSON.stringify(err);
 			callback(response, null);
 		} else {
 			response.body = JSON.stringify(data);
